03b59ffcab3399846834c356a3a376a38e939a1e
Yoichi Kawasaki <yokawasa@gmail.com>
2009-11-29 10:38:22 +0900

    fix: modules.mk

--- modules.mk
+++ modules.mk
@@ -1,9 +1,13 @@
-MOD_COUNT_MEMCOOKIE = mod_count_memcookie memcached_funcs\
+MOD_COUNT_MEMCOOKIE = mod_count_memcookie memcached_funcs
 
-HEADER = memcached_funcs.h \
+HEADER = memcached_funcs.h commons.h
 
-mod_count_memcookie.la: mod_count_memcookie.slo
-	$(SH_LINK) -rpath $(libexecdir) -module -avoid-version mod_count_memcookie.lo
+${MOD_COUNT_MEMCOOKIE:=.slo}: ${HEADER}
+${MOD_COUNT_MEMCOOKIE:=.lo}: ${HEADER}
+${MOD_COUNT_MEMCOOKIE:=.o}: ${HEADER}
+
+mod_count_memcookie.la: ${MOD_COUNT_MEMCOOKIE:=.slo}
+	$(SH_LINK) -rpath $(libexecdir) -module -avoid-version ${MOD_COUNT_MEMCOOKIE:=.lo}
 
 DISTCLEAN_TARGETS = modules.mk
 
